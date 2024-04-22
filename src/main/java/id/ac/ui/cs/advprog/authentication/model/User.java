4c4f1d68aa3d46ff676177dd1a3406eca81c66a7
sunsetheus <williamsalimrjs@gmail.com>
2024-04-23 02:25:46 +0700

    feat: add method from user details interface

--- src/main/java/id/ac/ui/cs/advprog/authentication/model/User.java
+++ src/main/java/id/ac/ui/cs/advprog/authentication/model/User.java
@@ -6,7 +6,9 @@ import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
+
 
 @Entity
 @Table(name = "users")
@@ -45,31 +47,31 @@ public class User implements UserDetails{
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return new ArrayList<>();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return this.email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
