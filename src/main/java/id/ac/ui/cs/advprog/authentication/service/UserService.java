f758bc42cf287e4d2d5a0937b2b118dd8acc7df7
sunsetheus <williamsalimrjs@gmail.com>
2024-04-23 02:34:32 +0700

    feat: add user service interface

---
+++ src/main/java/id/ac/ui/cs/advprog/authentication/service/UserService.java
@@ -0,0 +1,8 @@
+package id.ac.ui.cs.advprog.authentication.service;
+
+import id.ac.ui.cs.advprog.authentication.model.User;
+
+public interface UserService {
+
+    User registerUser(User user);
+}
